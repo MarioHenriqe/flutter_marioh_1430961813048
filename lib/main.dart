2655475e8394f6be799950c7d362ded16214c697
Mario Santos <mario.santos17@fatec.sp.gov.br>
2020-05-16 18:55:27 -0300

    Tarefa da segunda aula com Timer e inteligencia de inimigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/time.dart';
 
 void main() {
   runApp(GameWidget());
@@ -29,6 +30,7 @@ Paint _paint = Paint()..color = Color(0xFFFFFFFF);
 class GameObject {
   Rect position;
 
+
   void render(Canvas canvas){
     canvas.drawRect(position, _paint);
   }
@@ -38,27 +40,60 @@ class SpaceShooterGame extends Game{
 
   GameObject player;
 
+  Timer enemyCreator;
+
+  static const enemy_speed = 400;
+  List<GameObject> enemies = [];
+
+
   SpaceShooterGame(){
     player = GameObject()
         ..position = Rect.fromLTWH(200, 200, 100, 100);
+
+    enemyCreator = Timer(1.0, repeat: true, callback: (){
+
+      enemies.add(
+        GameObject()
+            ..position = Rect.fromLTWH(0, 0, 50, 50)
+
+      );
+
+    });
+    enemyCreator.start();
+
   }
 
   void OnPlayerMove(Offset delta){
 
-      player.position = player.position.translate(delta.dx, delta.dy);
+    player.position = player.position.translate(delta.dx, delta.dy);
 
   }
+
   
   @override
   void update(double dt){
+    enemies.forEach((enemy) {
 
+      enemy.position = enemy.position.translate(0, enemy_speed * dt);
+
+    });
+
+    enemyCreator.update(dt);
   }
 
   @override
   void render(Canvas canvas){
     
     player.render(canvas);
-    
+
+    enemies.forEach((enemy) {
+
+      enemy.render(canvas);
+
+    });
+
   }
+
+
 }
 
